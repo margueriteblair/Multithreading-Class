90d6d8f81ef020d6960dfe10031fcd5bbb06cee9
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-10 23:13:01 -0500

    thread scheduler

--- src/com/margieblair/DownloadFileTask.java
+++ src/com/margieblair/DownloadFileTask.java
@@ -5,5 +5,13 @@ public class DownloadFileTask implements Runnable{
     @Override
     public void run() {
         System.out.println("Downloading a file!" + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(5000); //this will suspend the current thread for 5 seconds
+        } catch(InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        //JVM has a virtual scheduler
+        System.out.println("Download Complete" + Thread.currentThread().getName());
     }
 }
