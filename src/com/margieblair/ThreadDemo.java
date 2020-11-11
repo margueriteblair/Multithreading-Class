cf787540aadbe53002c3e5d8c3144b39af018694
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-10 23:19:24 -0500

    thread joining to have a thread wait for another to be done before proceding with a task

--- src/com/margieblair/ThreadDemo.java
+++ src/com/margieblair/ThreadDemo.java
@@ -9,10 +9,22 @@ public class ThreadDemo {
     public static void show() {
         System.out.println(Thread.currentThread().getName());
         //each thread has a name and an id
-        for (int i = 0; i < 10; i++) {
+//        for (int i = 0; i < 10; i++) {
             Thread thread = new Thread(new DownloadFileTask());
             thread.start();
-        }
+            try {
+                thread.join();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        System.out.println("File is ready to be scanned.");
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+//        }
 
     }
 }
