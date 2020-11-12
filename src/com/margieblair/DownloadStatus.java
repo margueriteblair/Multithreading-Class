6a71c8eef3d066d30f0d71a4d8d6cd6d9eadcc00
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-11 23:50:16 -0500

    wait and notify

--- src/com/margieblair/DownloadStatus.java
+++ src/com/margieblair/DownloadStatus.java
@@ -4,11 +4,14 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class DownloadStatus {
-    private volatile boolean isDone;
+    private volatile boolean isDone; //we're telling the JVM, hey this object is unstable, don't rely on the data that is already stored in the cache
     private int totalBytes;
     private int totalFiles;
 //    private Lock lock = new ReentrantLock(); //this is an implementation of the lock interface
-
+//there are the wait() and notify() methods
+    //these are available for all objects in java
+    //it'll block the rest of the code until its data has been changed
+    //should be wrapped in a try/catch block
 
     public int getTotalBytes() {
         return totalBytes;
