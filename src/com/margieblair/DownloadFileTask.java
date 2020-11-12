902cb7ea9abac789455baf9df4b642ea16194d8e
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-11 19:49:48 -0500

    notes on multithreading

--- src/com/margieblair/DownloadFileTask.java
+++ src/com/margieblair/DownloadFileTask.java
@@ -13,6 +13,14 @@ public class DownloadFileTask implements Runnable{
             }
             System.out.println("Downloading byte" + i);
         }
+        //threads sometimes need to access shared resources
+        //if multiple threads access the same object and 1. of those threads modifies the object
+        //there are a few issues that you can run into
+        //1. multiple threads modifying the same data, the app can crash, this is a RACE CONDITION
+        //2. one thread changes the shared data, but the other threads can register the same data
+        //we see this issue in databases
+        //we need a proper understanding of these conditions
+        //thread-safe code is a must have
 //        try {
 //            Thread.sleep(5000); //this will suspend the current thread for 5 seconds
 //        } catch(InterruptedException ex) {
