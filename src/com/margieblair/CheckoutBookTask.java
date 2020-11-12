043b66f4de73c37b3c2f38d56f8389dd11fe50d1
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-11 23:56:12 -0500

    adding more to the library checkout system

--- src/com/margieblair/CheckoutBookTask.java
+++ src/com/margieblair/CheckoutBookTask.java
@@ -1,6 +1,10 @@
 package com.margieblair;
 
-public class CheckoutBookTask {
+public class CheckoutBookTask implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Currently checking out a book " + Thread.currentThread().getName());
+    }
     //would each thread checkout a different book? flesh out?
     //we need to talk about how different threads do work on the same data source in an efficient manner
     //perhaps an arraylist of the library's current books?
