d2056d7bc8c84513c8184c6cd1455bc415e77cda
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-11 22:16:57 -0500

    using a locking system to prevent race conditions

--- src/com/margieblair/DownloadStatus.java
+++ src/com/margieblair/DownloadStatus.java
@@ -1,13 +1,27 @@
 package com.margieblair;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DownloadStatus {
     private int totalBytes;
+    private Lock lock = new ReentrantLock(); //this is an implementation of the lock interface
+
 
     public int getTotalBytes() {
         return totalBytes;
     }
 
     public void incrementTotalBytes() {
-        totalBytes++;
+
+        lock.lock();
+        try {
+            totalBytes++;
+        } finally {
+            lock.unlock();
+            //make sure to unlock this lock inside a finally block
+            //still gets to 10000 bytes
+        }
+
     }
 }
