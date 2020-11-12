912cd70a083020879df9e74ab2744bd41eedbf0c
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-11 22:27:44 -0500

    outline of ideas for class

--- src/com/margieblair/CheckoutBookTask.java
+++ src/com/margieblair/CheckoutBookTask.java
@@ -1,4 +1,7 @@
 package com.margieblair;
 
 public class CheckoutBookTask {
+    //would each thread checkout a different book? flesh out?
+    //we need to talk about how different threads do work on the same data source in an efficient manner
+    //perhaps an arraylist of the library's current books?
 }
--- src/com/margieblair/CheckoutStatus.java
+++ src/com/margieblair/CheckoutStatus.java
@@ -1,4 +1,5 @@
 package com.margieblair;
 
 public class CheckoutStatus {
+    //this class will show the status of a book, if it's checked out or not
 }
