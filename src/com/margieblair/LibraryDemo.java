203f6249359fffee8fde6a8b0c7ed09768ccb472
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-11 22:53:19 -0500

    more prep for class

--- src/com/margieblair/LibraryDemo.java
+++ src/com/margieblair/LibraryDemo.java
@@ -1,4 +1,10 @@
 package com.margieblair;
 
 public class LibraryDemo {
+
+    public static void show() {
+        //this is where we'll put more specifics to actually run it
+        //then within the main class, LibraryDemo.show()
+        //should make a book object?
+    }
 }
