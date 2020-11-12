14610e292981ff612ac4695f5df8f51cc7177b0e
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-11 23:53:53 -0500

    made a constructor and a getter and setter for all the things in the book class

--- src/com/margieblair/Book.java
+++ src/com/margieblair/Book.java
@@ -5,5 +5,33 @@ public class Book {
     private String author;
     private boolean checkedIn;
 
+    public Book(String title, String author, boolean checkedIn) {
+        this.title = title;
+        this.author = author;
+        this.checkedIn = checkedIn;
+    }
 
+    public boolean isCheckedIn() {
+        return checkedIn;
+    }
+
+    public void setCheckedIn(boolean checkedIn) {
+        this.checkedIn = checkedIn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
