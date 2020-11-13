cbe8bcdf468ecb191729dff761f10e82f0dbbebd
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-12 20:22:27 -0500

    adder classes used instead of atomic classes to defeat race conditions

--- src/com/margieblair/DownloadStatus.java
+++ src/com/margieblair/DownloadStatus.java
@@ -1,25 +1,30 @@
 package com.margieblair;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class DownloadStatus {
     private volatile boolean isDone; //we're telling the JVM, hey this object is unstable, don't rely on the data that is already stored in the cache
-    private AtomicInteger totalBytes = new AtomicInteger();
+    private LongAdder totalBytes = new LongAdder();
     private int totalFiles;
 //    private Lock lock = new ReentrantLock(); //this is an implementation of the lock interface
 //there are the wait() and notify() methods
     //these are available for all objects in java
     //it'll block the rest of the code until its data has been changed
     //should be wrapped in a try/catch block
+    //adder classes are even faster than atomic types
+    //if we have something that'll be updated frequently, we should use an adder class
+    //LongAdder, DoubleAdder
 
     public int getTotalBytes() {
-        return totalBytes.get();
+        return totalBytes.intValue(); //internally will call sum();
     }
 
     public void incrementTotalBytes() {
-           totalBytes.incrementAndGet();
+           totalBytes.increment();
+           //same as atomic classes, there are lots of built in classes
            //atomic ints have an incrementAndGet() and also a getAndIncrement()
         //the order about returning a value or incrementing first is what matters
         //++a vs a++
