ec3e47dbce31837aab15c7c155ed1bdec570848d
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-19 16:32:23 -0500

    fleshed out main class with examples that intentionally wont work

--- src/bus/reservation/client/Test.java
+++ src/bus/reservation/client/Test.java
@@ -1,8 +1,17 @@
 package bus.reservation.client;
 
 
+import bus.reservation.system.TicketBookingThread;
+import bus.reservation.system.TicketCounter;
+
 public class Test {
     public static void main(String[] args) {
+        TicketCounter ticketCounter = new TicketCounter(); //doesn't take any args
+        TicketBookingThread t1 = new TicketBookingThread(ticketCounter, "Margie", 2);
+        TicketBookingThread t2 = new TicketBookingThread(ticketCounter, "Hello World", 2);
+        //both margie and hello world want 2 tickets, but there's only 3!
+        t1.start();
+        t2.start();
 
     }
 }
--- src/bus/reservation/system/TicketBookingThread.java
+++ src/bus/reservation/system/TicketBookingThread.java
@@ -2,7 +2,7 @@ package bus.reservation.system;
 
 public class TicketBookingThread extends Thread {
 
-    private TicketCounter ticketCounter;
+    private TicketCounter ticketCounter; //bc this references ticket counter, the number will automatically be 3 tickets to start
     private String passengerName;
     private int numOfSeatsToBook;
 
@@ -10,5 +10,9 @@ public class TicketBookingThread extends Thread {
         this.ticketCounter = ticketCounter;
         this.passengerName = passengerName;
         this.numOfSeatsToBook = numOfSeatsToBook;
+    }
+
+    public void run() {
+        ticketCounter.bookTicket(passengerName, numOfSeatsToBook);
     }
 }
