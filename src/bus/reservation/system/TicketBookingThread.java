b248b6b5396595be772b89db005985a015e0739a
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-19 16:29:22 -0500

    num seats to book and thread constructor made

--- src/bus/reservation/system/TicketBookingThread.java
+++ src/bus/reservation/system/TicketBookingThread.java
@@ -1,4 +1,14 @@
 package bus.reservation.system;
 
-public class TicketBookingThread {
+public class TicketBookingThread extends Thread {
+
+    private TicketCounter ticketCounter;
+    private String passengerName;
+    private int numOfSeatsToBook;
+
+    public TicketBookingThread(TicketCounter ticketCounter, String passengerName, int numOfSeatsToBook) {
+        this.ticketCounter = ticketCounter;
+        this.passengerName = passengerName;
+        this.numOfSeatsToBook = numOfSeatsToBook;
+    }
 }
