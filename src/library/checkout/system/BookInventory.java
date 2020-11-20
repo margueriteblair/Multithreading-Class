9b6bf9eab0678a4e7aa163d428be03485dae4eba
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-19 21:12:31 -0500

    made a library inventory of 10 books and created a checkout book method

--- src/library/checkout/system/BookInventory.java
+++ src/library/checkout/system/BookInventory.java
@@ -1,4 +1,14 @@
 package library.checkout.system;
 
 public class BookInventory {
+
+    private int availableBooks = 10;
+
+    public void checkoutBook(String cardName, int numOfBooks) {
+        if ((availableBooks >= numOfBooks) && (numOfBooks > 0)) {
+            System.out.println("Thanks, " + cardName + ", you've successfully checked out " + numOfBooks + " books.");
+        } else {
+            System.out.println("Unfortunately, those aren't all available");
+        }
+    }
 }
