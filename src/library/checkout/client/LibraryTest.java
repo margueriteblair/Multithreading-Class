480e280d3d8067059a3b81bf04b2057c1d557158
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-19 21:56:55 -0500

    note cannot use num initially greater than available book int

--- src/library/checkout/client/LibraryTest.java
+++ src/library/checkout/client/LibraryTest.java
@@ -1,15 +1,13 @@
 package library.checkout.client;
 
-import bus.reservation.system.TicketCounter;
-import com.margieblair.Book;
 import library.checkout.system.BookCheckoutThread;
 import library.checkout.system.BookInventory;
 
 public class LibraryTest {
     public static void main(String[] args) {
-        BookInventory bookInventory = new BookInventory();
+        BookInventory bookInventory = new BookInventory(); //doesn't take any args, but it'll provide us with a number of 10 books
         BookCheckoutThread threadBook1 = new BookCheckoutThread(bookInventory, "Margie", 7);
-        BookCheckoutThread threadBook2 = new BookCheckoutThread(bookInventory, "Hello world", 15);
+        BookCheckoutThread threadBook2 = new BookCheckoutThread(bookInventory, "Hello world", 5);
 
         threadBook1.start();
         threadBook2.start();
