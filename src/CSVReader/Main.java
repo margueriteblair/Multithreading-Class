f61ce4c6a8606392456971178db75181559d67cb
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-13 21:57:40 -0500

    all animes printing out

--- src/CSVReader/Main.java
+++ src/CSVReader/Main.java
@@ -5,9 +5,11 @@ import java.io.FileReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Working!");
         String path = "/Users/margueriteblair/Desktop/anime.csv";
-
+        String line = "";
         BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        while ((line = bufferedReader.readLine()) != null) {
+            System.out.println(line);
+        }
     }
 }
