a10f495ad95d8f30b46066ef8c15214aca7145ae
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-13 22:25:11 -0500

    copied it over into threading section

--- src/CSVReader/MainWithThreading.java
+++ src/CSVReader/MainWithThreading.java
@@ -1,7 +1,23 @@
 package CSVReader;
 
-public class MainWithThreading {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
 
+public class MainWithThreading {
+    public static void main(String[] args) throws Exception {
+        long startTime = System.nanoTime();
+        String path = "/Users/margueriteblair/Desktop/anime.csv";
+        String line = "";
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        bufferedReader.readLine();
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] values = line.split(",");
+            System.out.println(values[1] + " is a " + values[2] + " and has a rating of " + values[5]);
+        }
+        long endTime = System.nanoTime();
+        long durationInMilliseconds = (endTime - startTime) / 1000000;
+        //prev: 485189006
+        //current: 511 milliseconds
+        System.out.println("The process without threading ran in: " + durationInMilliseconds + " milliseconds");
     }
 }
