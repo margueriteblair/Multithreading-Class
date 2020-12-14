e15cb233030aa5ee31ff0f96c726b33ebc3ac865
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-13 22:17:48 -0500

    going to create a constrasting example that uses threading to get the job done faster

--- src/CSVReader/Main.java
+++ src/CSVReader/Main.java
@@ -6,13 +6,19 @@ import java.io.FileReader;
 public class Main {
     //link to .csv file: https://www.kaggle.com/CooperUnion/anime-recommendations-database
     public static void main(String[] args) throws Exception {
+        long startTime = System.nanoTime();
         String path = "/Users/margueriteblair/Desktop/anime.csv";
         String line = "";
         BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
         bufferedReader.readLine();
         while ((line = bufferedReader.readLine()) != null) {
             String[] values = line.split(",");
-            System.out.println(values[0]);
+            System.out.println(values[1] + " is a " + values[2] + " and has a rating of " + values[5]);
         }
+        long endTime = System.nanoTime();
+        long durationInMilliseconds = (endTime - startTime) / 1000000;
+        //prev: 485189006
+        //current: 511 milliseconds
+        System.out.println("The process without threading ran in: " + durationInMilliseconds + " milliseconds");
     }
 }
---
+++ src/CSVReader/MainWithThreading.java
@@ -0,0 +1,7 @@
+package CSVReader;
+
+public class MainWithThreading {
+    public static void main(String[] args) {
+
+    }
+}
