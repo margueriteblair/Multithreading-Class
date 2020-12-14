1489aa97ab6cd502ff5423abdf8e371969f326b9
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-13 22:00:38 -0500

    skipping the column header

--- src/CSVReader/Main.java
+++ src/CSVReader/Main.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class Main {
+    //link to .csv file: https://www.kaggle.com/CooperUnion/anime-recommendations-database
     public static void main(String[] args) throws Exception {
         String path = "/Users/margueriteblair/Desktop/anime.csv";
         String line = "";
         BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
+        bufferedReader.readLine();
         while ((line = bufferedReader.readLine()) != null) {
-            System.out.println(line);
+            String[] values = line.split(",");
+            System.out.println(values[0]);
         }
     }
 }
