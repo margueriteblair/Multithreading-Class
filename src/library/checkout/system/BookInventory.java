73effb53422d70c74ba4d50cbb689662b5f4ec37
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-14 23:44:47 -0500

    long adder

--- src/library/checkout/system/BookInventory.java
+++ src/library/checkout/system/BookInventory.java
@@ -1,28 +1,22 @@
 package library.checkout.system;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.atomic.LongAdder;
 
 public class BookInventory {
 
     private int availableBooks = 10;
-    private Lock lock = new ReentrantLock();
+    private LongAdder txCounter = new LongAdder();
 
     public void checkoutBook(String cardName, int numOfBooks) {
-        lock.lock();
-        try {
-            if ((availableBooks >= numOfBooks) && (numOfBooks > 0)) {
-                System.out.println("Thanks, " + cardName + ", you've successfully checked out " + numOfBooks + " books.");
-                availableBooks = (availableBooks - numOfBooks);
-            } else {
-                System.out.println("Unfortunately, those aren't all available");
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            lock.unlock();
-        }
 
+        if ((availableBooks >= numOfBooks) && (numOfBooks > 0)) {
+            txCounter.increment();
+            System.out.println("Thanks, " + cardName + ", you've successfully checked out " + numOfBooks + " books.");
+            System.out.println("The library has had a total of " + txCounter + " checkouts today!");
+            availableBooks = (availableBooks - numOfBooks);
+        } else {
+            System.out.println("Unfortunately, those aren't all available");
+        }
     }
 }
