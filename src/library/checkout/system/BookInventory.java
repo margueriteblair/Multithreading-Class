e1f6028288dfd6302a66fe19132310807ded278a
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-14 23:35:58 -0500

    lock example

--- src/library/checkout/system/BookInventory.java
+++ src/library/checkout/system/BookInventory.java
@@ -1,19 +1,28 @@
 package library.checkout.system;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class BookInventory {
 
-    private AtomicInteger availableBooks = new AtomicInteger(10);
+    private int availableBooks = 10;
+    private Lock lock = new ReentrantLock();
 
-    //ways to prevent race conditions:
-    //1. synchronized key word
-    public synchronized void checkoutBook(String cardName, int numOfBooks) {
-        if ((availableBooks.get() >= numOfBooks) && (numOfBooks > 0)) {
-            System.out.println("Thanks, " + cardName + ", you've successfully checked out " + numOfBooks + " books.");
-            availableBooks.set(availableBooks.get() - numOfBooks);
-        } else {
-            System.out.println("Unfortunately, those aren't all available");
+    public void checkoutBook(String cardName, int numOfBooks) {
+        lock.lock();
+        try {
+            if ((availableBooks >= numOfBooks) && (numOfBooks > 0)) {
+                System.out.println("Thanks, " + cardName + ", you've successfully checked out " + numOfBooks + " books.");
+                availableBooks = (availableBooks - numOfBooks);
+            } else {
+                System.out.println("Unfortunately, those aren't all available");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            lock.unlock();
         }
+
     }
 }
