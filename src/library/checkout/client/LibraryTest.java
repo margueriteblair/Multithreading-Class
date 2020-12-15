bd86e4873e7b19e42c087ae6d46100724d1730b0
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-14 23:53:45 -0500

    join

--- src/library/checkout/client/LibraryTest.java
+++ src/library/checkout/client/LibraryTest.java
@@ -4,12 +4,13 @@ import library.checkout.system.BookCheckoutThread;
 import library.checkout.system.BookInventory;
 
 public class LibraryTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BookInventory bookInventory = new BookInventory(); //doesn't take any args, but it'll provide us with a number of 10 books
         BookCheckoutThread threadBook1 = new BookCheckoutThread(bookInventory, "Margie", 7);
         BookCheckoutThread threadBook2 = new BookCheckoutThread(bookInventory, "Hello world", 5);
 
         threadBook1.start();
+        threadBook1.join();
         threadBook2.start();
     }
 }
--- src/library/checkout/system/BookInventory.java
+++ src/library/checkout/system/BookInventory.java
@@ -1,19 +1,15 @@
 package library.checkout.system;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.LongAdder;
 
 public class BookInventory {
 
     private int availableBooks = 10;
-    private LongAdder txCounter = new LongAdder();
 
     public void checkoutBook(String cardName, int numOfBooks) {
 
         if ((availableBooks >= numOfBooks) && (numOfBooks > 0)) {
-            txCounter.increment();
             System.out.println("Thanks, " + cardName + ", you've successfully checked out " + numOfBooks + " books.");
-            System.out.println("The library has had a total of " + txCounter + " checkouts today!");
             availableBooks = (availableBooks - numOfBooks);
         } else {
             System.out.println("Unfortunately, those aren't all available");
